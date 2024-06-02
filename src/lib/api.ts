095f6452d7f04b77e40c12aa1189b5c86cf2d900
Le Nguyen <lenguyen18072003@gmail.com>
2024-06-02 22:50:31 +0700

    perf: change from fetch to axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,22 +1,32 @@
 import { Book, User, Library } from "@/lib/interface";
 import { ResolveURL } from "@/lib/utils";
+import axios from 'axios';
 
-/**
- * 
- * @returns Trả về 10 sách được query từ database
- */
+interface GetBookProps {
+    limit: number,
+    page: number,
+    sort_by?: string | undefined,
+    genres?: string[] | undefined,
+    publisher?: string | undefined,
+    language?: string | undefined
+};
+
 export async function GetBooks() {
-    const get = await fetch(ResolveURL("books"), {
-        method: 'GET',
-    });
-    const response = await get.json();
-    return response as Book[];
+    const res = await axios.get(ResolveURL("books"));
+    return res.data as Book[];
 }
 
 export async function GetBookByID(ID: string) {
-    const get = await fetch(ResolveURL(`books/${ID}`), {
-        method: 'GET',
-    });
-    const response = await get.json();
-    return response as Book;
+    const res = await axios.get(ResolveURL(`books/${ID}`));
+    return res.data as Book;
+}
+
+export async function GetBooksParam({...props} : GetBookProps ) {
+    const res = await axios.get(ResolveURL("books/"), { params: props });
+    return res.data as Book[];
+}
+
+export async function GetLibraryByID(ID: string) {
+    const res = await axios.get(ResolveURL(`libraries/${ID}`));
+    return res.data as Library;
 }
