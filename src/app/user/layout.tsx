5d17622f22ad0e6dfb4002d3e3739a72b0df4fd3
Le Nguyen <lenguyen18072003@gmail.com>
2024-06-02 22:47:24 +0700

    refactor: add style to user layout

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -9,7 +9,13 @@ export default function UserLayout({
     <>
       <SiteHeader />
       <main className="border-t">
-        {children}
+        <div className="lg:border-l w-screen">
+          <div className="h-full px-4 py-6 lg:px-8">
+            <div className="border-md p-0 outline-none">
+              {children}
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
