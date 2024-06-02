6925b1a636716864a3a21a620a89c176f4145345
Le Nguyen <lenguyen18072003@gmail.com>
2024-06-02 22:50:41 +0700

    add util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,21 @@ export function ResolveURL(path: string) {
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/${path}`
   : `http://127.0.0.1:8000/api/${path}`;
 }
+
+export function ToDateString(date: string) {
+  return (new Date(date)).toLocaleDateString("vi-VN", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+  });
+}
+
+export function ToDateID(id: string) {
+  const timestamp = id.substring(0, 8);
+  const date = new Date( parseInt(timestamp, 16) * 1000)
+  return date.toLocaleDateString("vi-VN", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+  });
+}
