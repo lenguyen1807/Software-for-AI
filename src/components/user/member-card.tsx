4add75faad62b6e8b5ce1d36f9d44c2896829e79
vmthu-xii <vmthu.xii@gmail.com>
2024-06-10 13:55:23 +0700

    feat: add member-card component

---
+++ src/components/user/member-card.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { CalendarDays, X } from "lucide-react"
+import { Card } from "@/components/ui/card"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+
+export default function MemberCard({ lib }: { lib: string }) {
+    return (
+        <Card className="relative p-[20px] hover:bg-gray-100/50 cursor-pointer">
+            <div className="grid grid-cols-10 flex space-x-[30px] ml-2 items-center">
+                <Avatar className="ring-1 ring-slate-200">
+                    <AvatarImage src="" />
+                    <AvatarFallback>T</AvatarFallback>
+                </Avatar>
+                <div className="space-y-2 col-span-8">
+                    <h4 className="text-sm font-semibold">{lib}</h4>
+
+                    <p className="text-sm text-muted-foreground">
+                        Nguyễn Văn Cừ, Quận 5, Hồ Chí Minh, Hà Nội, Hồ Chí Minh
+                    </p>
+
+                    <div className="flex items-center pt-2">
+                        <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                        <span className="text-xs text-muted-foreground">
+                            Thành viên từ 12/12/2021
+                        </span>
+                    </div>
+                </div>
+                <Button variant="ghost" className="absolute right-2 hover:bg-transparent group">
+                    <X size={15} strokeWidth={2} className="group-hover:stroke-red-500 stroke-gray-500" />
+                </Button>
+            </div>
+        </Card>
+
+        // <div className="flex space-x-4 ml-2">
+        //     <Avatar>
+        //         <AvatarImage src={lib?.avatarImageUrl} />
+        //         <AvatarFallback>Avatar thư viện</AvatarFallback>
+        //     </Avatar>
+        //     <div className="space-y-1">
+        //         <h4 className="text-sm font-semibold">{lib?.name}</h4>
+        //         <p className="text-sm text-muted-foreground">
+        //             {lib?.address}
+        //         </p>
+        //         <div className="flex items-center pt-2">
+        //             <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+        //             <span className="text-xs text-muted-foreground">
+        //                 Thành viên từ 12/12/2021
+        //             </span>
+        //         </div>
+        //     </div>
+        // </div>
+    )
+}
