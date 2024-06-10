4f1133e34cde958a6c62cd0737c851767e4cc454
vmthu-xii <vmthu.xii@gmail.com>
2024-06-10 21:40:40 +0700

    add api get user's libs & borrows

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -35,3 +35,23 @@ export async function GetLoginToken({...props} : {username: string, password: st
     const res = await axios.post(ResolveURL(`login`), null, { params: props});
     return res.data.access_token;
 }
+
+export async function GetUserLibrary() {
+    const res = await axios.get(ResolveURL("user/libraries"), {
+                        method: "GET",
+                        headers: {
+                            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTgxMDA0MzMuNzcwNjkzMywiaWQiOiI2MGQ5ZjRmMWUxYTNlNTZhM2MzZjNiM2YiLCJyb2xlIjoidXNlciJ9.xJqDaUTyGsXlt6rqREvr8wq0-PLrRrp40jWXzYDveOo`
+                        }
+                    });
+    return res.data as Library[];
+}
+
+export async function GetUserBorrows() {
+    const res = await axios.get(ResolveURL("user/borrows"), {
+                        method: "GET",
+                        headers: {
+                            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTgxMDA0MzMuNzcwNjkzMywiaWQiOiI2MGQ5ZjRmMWUxYTNlNTZhM2MzZjNiM2YiLCJyb2xlIjoidXNlciJ9.xJqDaUTyGsXlt6rqREvr8wq0-PLrRrp40jWXzYDveOo`
+                        }
+                    });
+    return res.data as Book[];
+}
