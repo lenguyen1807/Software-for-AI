5a26155690d5c98c704fe070a1772725d633f8ba
VipProCute <anhtunguyen2112003@gmail.com>
2024-06-21 23:52:18 +0700

    Create columns.tsx

---
+++ src/components/library/service-table/borrow/columns.tsx
@@ -0,0 +1,180 @@
+"use client"
+
+import { BorrowHistory } from "@/lib/interface";
+import { ColumnDef } from "@tanstack/react-table";
+import { ToDateID } from "@/lib/utils";
+import { ArrowUpDown, SquareCheck, Trash2 } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useSession } from "next-auth/react";
+
+export const getColumns = ({onDelete} : {onDelete: (id: string, token: string) => void}):ColumnDef<BorrowHistory>[] => [
+    {
+        accessorKey: "borrowDate",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center items-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Ngày nhận
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+
+            )
+        },
+        cell: ({ row }) => <div className="text-center">{ToDateID(row.getValue("borrowDate"))}</div>,
+    },
+    {
+        accessorKey: "bookID",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center items-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Mã sách
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+
+            )
+        },
+        cell: ({ row }) => <div className="text-center">{row.getValue("bookID")}</div>,
+    },
+    {
+        accessorKey: "userID",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center items-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Mã người đặt
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+
+            )
+        },
+        cell: ({ row }) => <div className="text-center">{row.getValue("userID")}</div>,
+    },
+    {
+        accessorKey: "status",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center items-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Trạng thái
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+
+            )
+        },
+        cell: ({ row }) => <div className="text-center">{row.getValue("status")}</div>,
+    },
+    /*{
+        accessorKey: "note",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center items-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Ghi chú
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+
+            )
+        },
+        cell: ({ row }) => <div className="text-center">{row.getValue("note"))}</div>,
+    },*/
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            const { data: session, status } = useSession();
+            const token = session?.user.jwt;
+
+            return (
+                <AlertDialog>
+                    <AlertDialogTrigger>
+                        <Button variant="ghost"><SquareCheck size={16} /></Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Bạn có chắc chắn muốn xóa sách này?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Hành động này không thể được hoàn tác và sẽ xóa vĩnh viễn dữ liệu của sách khỏi máy chủ của chúng tôi.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Huỷ</AlertDialogCancel>
+                            <AlertDialogAction
+                                onClick={
+                                    async() => onDelete(row.getValue("_id"), token)
+                                }
+                            >
+                                Tiếp tục
+                            </AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            )
+        },
+    },
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            const { data: session, status } = useSession();
+            const token = session?.user.jwt;
+
+            return (
+                <AlertDialog>
+                    <AlertDialogTrigger>
+                        <Button variant="ghost"><Trash2 size={16} /></Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Bạn có chắc chắn muốn xóa sách này?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Hành động này không thể được hoàn tác và sẽ xóa vĩnh viễn dữ liệu của sách khỏi máy chủ của chúng tôi.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Huỷ</AlertDialogCancel>
+                            <AlertDialogAction
+                                onClick={
+                                    async() => onDelete(row.getValue("_id"), token)
+                                }
+                            >
+                                Tiếp tục
+                            </AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            )
+        },
+    },
+];
