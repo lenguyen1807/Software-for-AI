463b15a06f51f5df6da470b3379c3936b0b378d0
VipProCute <anhtunguyen2112003@gmail.com>
2024-06-21 23:54:03 +0700

    Update layout.tsx

--- src/app/library/layout.tsx
+++ src/app/library/layout.tsx
@@ -1,26 +1,30 @@
+import { Nunito } from "next/font/google";
+import SideBar from "@/components/library/lib_sidebar";
+import { cn } from "@/lib/utils";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export default async function UserLayout({
+const nunito = Nunito({ subsets: ["vietnamese"] });
+
+export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = (await auth())?.user;
 
-    const user = (await auth())?.user;
+  if (user) {
+      if (user.role != "library") {
+          redirect("/");
+      }
+  } else {
+      redirect("/login");
+  }
 
-    if (user) {
-        if (user.role != "library") {
-            redirect("/");
-        }
-    } else {
-        redirect("/login");
-    }
-
-    return (
-        <>
+  return (
+    <div className={cn(nunito.className, "w-full flex w-screen pl-[250px]")}>
+        <SideBar />
         {children}
-        </>
-    )
-
+    </div>
+  );
 }
