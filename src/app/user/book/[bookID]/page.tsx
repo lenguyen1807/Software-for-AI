9044003e16e4edc1f502d94ea979d9481789debd
vmthu-xii <vmthu.xii@gmail.com>
2024-06-27 09:38:03 +0700

    ui: update responsive

--- src/app/user/book/[bookID]/page.tsx
+++ src/app/user/book/[bookID]/page.tsx
@@ -6,7 +6,7 @@ import { STIX_Two_Text, Nunito, Playfair_Display } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { auth } from "@/lib/auth";
 import { GetBookByID, GetBookReviews, GetLibraryByID, GetUserInfo, GetUserLibrary } from "@/lib/api";
-import { RatingBar } from "@/components/user/rating-bar";
+
 import { Review } from "@/components/user/review";
 import ReviewForm from "@/components/user/review-form";
 import { BookReview } from "@/lib/interface";
@@ -33,31 +33,33 @@ type Props = {
 function LinkField({
   label,
   value,
-  href
+  href,
 }: {
   label: string,
   value: string,
-  href: string
+  href: string,
 }) {
   return (
-    <div>
-      <h2 className="text-slate-700 text-lg">
+    <div className="space-y-3">
+      <h2 className="text-slate-700 text-base">
         {label}
       </h2>
-      <Link
-        className={cn(nunito.className, "text-muted-foreground text-pretty hover:underline underline-offset-8")}
-        href={href}
-      >
-        <span>{value}</span>
-      </Link>
-    </div>
+      <div>
+        <Link
+          className={cn(nunito.className, "text-muted-foreground text-pretty hover:underline underline-offset-8")}
+          href={href}
+        >
+          <span>{value}</span>
+        </Link>
+      </div>
+    </div >
   )
 }
 
 function TextField({ label, value }: { label: string, value: string }) {
   return (
-    <div>
-      <h2 className="text-slate-700 text-lg">
+    <div className="space-y-3">
+      <h2 className="text-slate-700 text-base">
         {label}
       </h2>
       <p className={cn(nunito.className, "text-muted-foreground text-pretty text-justify")}>
@@ -83,8 +85,8 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="space-y-12">
-      <div className="-space-y-28">
-        <div className='md:grid md:grid-flow-col md:grid-cols-2'>
+      <div className="lg:-space-y-[90px]">
+        <div className='lg:grid lg:grid-flow-col lg:grid-cols-2 gap-x-6'>
           <div className="flex items-center justify-center">
             <div
               className="overflow-hidden flex rounded-r-xl shadow-[-4px_4px_rgba(150,150,150,_0.3),_-8px_8px_rgba(150,150,150,_0.25),_-12px_12px_rgba(150,150,150,_0.2),_-16px_16px_rgba(150,150,150,_0.15),_-20px_20px_rgba(150,150,150,_0.1),_-24px_24px_rgba(150,150,150,_0.05)]"
@@ -100,8 +102,8 @@ export default async function Page({ params }: Props) {
               />
             </div>
           </div>
-          <div className="pt-6">
-            <div className="space-y-5">
+          <div className="flex flex-col py-6 h-full justify-between">
+            <div className="flex flex-col space-y-4 lg:space-y-6">
               {book.series != null &&
                 <div className="text-slate-600">
                   <div className="grid grid-cols-1 grid-flow-row">
@@ -117,7 +119,7 @@ export default async function Page({ params }: Props) {
                   </div>
                 </div>
               }
-              <h1 className={cn("text-5xl text-wrap", stix.className)}>
+              <h1 className={cn("text-4xl lg:text-5xl text-wrap", stix.className)}>
                 {book.title}
               </h1>
               <div className={cn("flex items-center gap-x-20 text-base text-wrap h-fit", nunito.className)}>
@@ -136,8 +138,8 @@ export default async function Page({ params }: Props) {
                   {book.numPages} trang
                 </span>
               </div>
-              <div className="flex items-center space-x-5">
-                {/* <Ratings
+              {/* <div className="flex items-center space-x-5"> */}
+              {/* <Ratings
                   rating={book.avgRating}
                   totalStars={5}
                   variant="yellow"
@@ -146,13 +148,13 @@ export default async function Page({ params }: Props) {
                   disabled={true}
                 />
                 <span className={cn("text-lg", nunito.className)}>{book.avgRating}/5</span> */}
-                {/* <span className="text-sm text-muted-foreground">{book.numOfRating} ratings</span> */}
-              </div>
+              {/* <span className="text-sm text-muted-foreground">{book.numOfRating} ratings</span> */}
+              {/* </div> */}
               <div className="flex flex-wrap gap-4 items-center">
                 {book.genres.map((genre) => (
                   <Link
                     key={genre}
-                    className="text-muted-foreground hover:text-primary underline-offset-8 underline cursor-pointer"
+                    className="text-sm underline-offset-4 lg:text-base lg:underline-offset-8 text-muted-foreground hover:text-primary  underline cursor-pointer"
                     href={`/user/explore/genre/${genre}`}
                   >
                     {genre}
@@ -160,11 +162,7 @@ export default async function Page({ params }: Props) {
                 ))}
               </div>
             </div>
-          </div>
-        </div>
-        <div className="bg-white px-12 pb-12 pt-4 rounded-2xl shadow-[rgba(50,50,93,0.08)_0px_0px_12px_-2px,_rgba(0,0,0,0.08)_0px_3px_7px_-3px]">
-          <div className="md:grid md:grid-cols-2">
-            <div className="pt-6 md:col-start-2">
+            <div className="pt-6">
               <BorrowForm
                 book={book}
                 libs={libs}
@@ -176,14 +174,16 @@ export default async function Page({ params }: Props) {
               />
             </div>
           </div>
-          <div className="md:grid md:grid-cols-2 justify-between w-full gap-32 pt-20">
-            <div className="md:col-start-1">
+        </div>
+        <div className="bg-white px-12 pb-12 pt-12 lg:pt-32 rounded-2xl shadow-[rgba(50,50,93,0.08)_0px_0px_12px_-2px,_rgba(0,0,0,0.08)_0px_3px_7px_-3px]">
+          <div className="lg:grid lg:grid-cols-2 justify-between w-full lg:gap-24 flex flex-col gap-y-10">
+            <div className="lg:col-start-1">
               <TextField
                 label="Mô tả"
                 value={book.description == null ? "Không có" : book.description}
               />
             </div>
-            <div className="md:grid md:grid-cols-2 grid-rows-3 justify-between gap-y-10">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-10 flex flex-col gap-y-10 justify-between">
               <LinkField
                 label="Thư viện"
                 value={lib.name}
@@ -217,38 +217,38 @@ export default async function Page({ params }: Props) {
       </div >
       <div className={cn("space-y-4", nunito.className)}>
         <span className="text-lg font-bold" >
-          Bạn đọc đánh giá
+          Đánh giá trải nghiệm của bạn
         </span>
-        <div className="grid grid-cols-2 gap-x-20">
-          <div className=" bg-white px-12 py-12 rounded-2xl shadow-[rgba(50,50,93,0.08)_0px_0px_12px_-2px,_rgba(0,0,0,0.08)_0px_3px_7px_-3px]">
-            <div className="flex md:flex-row flex-col items-center justify-around md:gap-0 gap-10">
-              <div className="shrink-0 flex flex-col space-y-3 items-center">
-                <span className="text-2xl font-bold">{book.avgRating} / 5</span>
-                  <Ratings
-                    rating={book.avgRating}
-                    totalStars={5}
-                    variant="yellow"
-                    className="flex"
-                    size={20}
-                    disabled={true}
-                  />
-                <span className="text-sm text-muted-foreground">({book.numOfRating} đánh giá)</span>
-              </div>
+        <div className="grid grid-cols-3 gap-6">
+          <div className="col-span-3 md:col-span-1  bg-white p-6 rounded-2xl shadow-[rgba(50,50,93,0.08)_0px_0px_12px_-2px,_rgba(0,0,0,0.08)_0px_3px_7px_-3px]">
+            <div className="flex flex-col h-full space-y-3 justify-center items-center ">
+              <span className="text-2xl font-bold">{book.avgRating} / 5</span>
+              <Ratings
+                rating={book.avgRating}
+                totalStars={5}
+                variant="yellow"
+                className="flex"
+                size={20}
+                disabled={true}
+              />
+              <span className="text-sm text-muted-foreground">({book.numOfRating} đánh giá)</span>
               {/* <div className="w-[70%]">
                 <RatingBar />
               </div> */}
             </div>
           </div>
-          <ReviewForm 
-            bookID={book._id} 
-            userID={user?.id} 
-          />
+          <div className="col-span-3 md:col-span-2 p-6 bg-white rounded-2xl shadow-[rgba(50,50,93,0.08)_0px_0px_12px_-2px,_rgba(0,0,0,0.08)_0px_3px_7px_-3px]">
+            <ReviewForm
+              bookID={book._id}
+              userID={user?.id}
+            />
+          </div>
         </div>
       </div>
 
       <div className={cn("space-y-4", nunito.className)}>
         <span className="text-lg font-bold" >
-          Nhận xét
+          Bạn đọc nhận xét
         </span>
         <div className="flex flex-col space-y-6">
           {reviews.reverse().map((review: BookReview) => (
