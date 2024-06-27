c08d608181eaf38ca8bf2a411bdcb8bf27b58185
vmthu-xii <vmthu.xii@gmail.com>
2024-06-27 10:52:29 +0700

    feat: update create-account

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,24 @@
-export default function Page() {
+import { auth } from "@/lib/auth";
+import { ResolveURL } from "@/lib/utils";
+import { User } from "@/lib/interface";
+import AccountAdminTable from "@/components/admin/account-table/table";
+import { SessionProvider } from "next-auth/react";
+
+export default async function Page() {
+    const token = (await auth())?.user.jwt;
+
+    const res = await fetch(ResolveURL("user?get_all=true"), {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    const data = (await res.json()) as User[];
+
     return (
-        <main>
-        </main>
-    );
+        <SessionProvider>
+            <AccountAdminTable data={data} />
+        </SessionProvider>
+    )
 }
